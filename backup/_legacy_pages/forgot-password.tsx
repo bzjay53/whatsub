c21df5f65f42878cbccdf1eb8ef811a51e1fead4
bzjay53 <bajay53@gmail.com>
2025-03-25 09:35:39 +0900

    확실한 Netlify 배포 설정 - 백업 제거 및 빌드 설정 최적화

--- backup/_legacy_pages/forgot-password.tsx
+++
@@ -1,4 +0,0 @@
-<Head>
-  <title>비밀번호 찾기 - Whatsub</title>
-  <meta name="description" content="Whatsub 계정 비밀번호를 찾습니다." />
-</Head> 
--- backup/_pages_backup/dashboard.tsx
+++
@@ -1,4 +0,0 @@
-<Head>
-  <title>대시보드 - Whatsub</title>
-  <meta name="description" content="Whatsub AI 자막 서비스 대시보드" />
-</Head> 
